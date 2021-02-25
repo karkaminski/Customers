6ca83178d6d47feac0b2d6722738549a5970eb3c
karol <karkaminski@gmail.com>
2021-02-25 22:42:29 +0100

    sortowanie tabeli Classifications

--- app/src/main/java/pl/karkaminski/customers/ui/classifications/ClassificationsFragment.java
+++ app/src/main/java/pl/karkaminski/customers/ui/classifications/ClassificationsFragment.java
@@ -12,13 +12,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import de.codecrafters.tableview.model.TableColumnWeightModel;
+import de.codecrafters.tableview.toolkit.SimpleTableHeaderAdapter;
 import pl.karkaminski.customers.database.CustomerClassification;
 import pl.karkaminski.customers.databinding.ClassificationsFragmentBinding;
 
 public class ClassificationsFragment extends Fragment {
+
+    public static final String[] TABLE_HEADERS = {"ID", "Name", "Description"};
 
     private ClassificationsViewModel mViewModel;
     private ClassificationsFragmentBinding binding = null;
@@ -31,10 +35,14 @@ public class ClassificationsFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(ClassificationsViewModel.class);
 
         TableColumnWeightModel columnModel = new TableColumnWeightModel(3);
-        columnModel.setColumnWeight(0, 1);
+        columnModel.setColumnWeight(0, 3);
         columnModel.setColumnWeight(1, 4);
         columnModel.setColumnWeight(2, 6);
         binding.tableView.setColumnModel(columnModel);
+        binding.tableView.setColumnComparator(0, new ClassificationIdComparator());
+        binding.tableView.setColumnComparator(1, new ClassificationNameComparator());
+        binding.tableView.setEmptyDataIndicatorView(binding.textViewNoData);
+        binding.tableView.setHeaderAdapter(new SimpleTableHeaderAdapter(getContext(), TABLE_HEADERS));
 
         List<CustomerClassification> customerClassifications = new ArrayList<>();
         CustomerClassificationTableDataAdapter adapter = new CustomerClassificationTableDataAdapter(getContext(), customerClassifications);
@@ -56,5 +64,19 @@ public class ClassificationsFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         binding = null;
+    }
+
+    private static class ClassificationNameComparator implements Comparator<CustomerClassification>{
+        @Override
+        public int compare(CustomerClassification cc1, CustomerClassification cc2) {
+            return cc1.getName().compareTo(cc2.getName());
+        }
+    }
+
+    private static class ClassificationIdComparator implements Comparator<CustomerClassification> {
+        @Override
+        public int compare(CustomerClassification cc1, CustomerClassification cc2) {
+            return String.valueOf(cc1.getId()).compareTo(String.valueOf(cc2.getId()));
+        }
     }
 }
